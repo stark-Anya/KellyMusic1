ad563082ec39802a732c1b955a820bc15a87dfdf
stark-Anya <misterxstark07@gmail.com>
2026-04-11 19:31:23 +0530

    Update play.py

--- ShrutiMusic/utils/inline/play.py
+++ ShrutiMusic/utils/inline/play.py
@@ -1,23 +1,4 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
+
 
 
 import math
@@ -54,25 +35,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "🅐—————————"
+        bar = "🅚—————————"
     elif 10 < umm < 20:
-        bar = "—🅝————————"
+        bar = "—🅔————————"
     elif 20 <= umm < 30:
-        bar = "——🅨———————"
+        bar = "——🅛———————"
     elif 30 <= umm < 40:
-        bar = "———🅐——————"
+        bar = "———🅛——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————🅨—————"
     elif 50 <= umm < 60:
-        bar = "—————🅑————"
+        bar = "—————🅜————"
     elif 60 <= umm < 70:
-        bar = "——————🅞———"
+        bar = "——————🅤———"
     elif 70 <= umm < 80:
-        bar = "———————🅣——"
+        bar = "———————🅢——"
     elif 80 <= umm < 95:
-        bar = "————————🅢—"
+        bar = "————————🅘—"
     else:
-        bar = "—————————◉"
+        bar = "—————————🅒"
 
     buttons = [
         [
@@ -82,7 +63,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="▶", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
@@ -100,7 +81,7 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▶", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
