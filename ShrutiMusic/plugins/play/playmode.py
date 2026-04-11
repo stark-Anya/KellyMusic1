573f50d97a26b0fae5eef46ba6dd08014bfc29c2
stark-Anya <misterxstark07@gmail.com>
2026-04-11 18:57:56 +0530

    Update playmode.py

--- ShrutiMusic/plugins/play/playmode.py
+++ ShrutiMusic/plugins/play/playmode.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
 
 
 from pyrogram import filters
@@ -54,14 +34,3 @@ async def playmode_(client, message: Message, _):
         reply_markup=InlineKeyboardMarkup(buttons),
     )
 
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
