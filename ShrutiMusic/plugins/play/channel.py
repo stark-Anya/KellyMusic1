ab99e403869666bd9e5bfda48813b56131339866
stark-Anya <misterxstark07@gmail.com>
2026-04-11 18:57:04 +0530

    Update channel.py

--- ShrutiMusic/plugins/play/channel.py
+++ ShrutiMusic/plugins/play/channel.py
@@ -1,23 +1,4 @@
 # Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
 
 
 from pyrogram import filters
@@ -71,13 +52,3 @@ async def playmode_(client, message: Message, _):
         return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
 
 
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
